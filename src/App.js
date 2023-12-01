f09e96d04a189acfbcb66a059e8d01ab9e7da205
Samuel Hansen <samuelhansen16@gmail.com>
2023-11-30 20:00:47 -0700

    deleted comments, whoops

--- src/App.js
+++ src/App.js
@@ -1,62 +1,3 @@
-// import React, { useState } from 'react';
-// import './App.css';
-
-
-// function App() {
-//   // State hook - 'useState'
-//   const [newItem, setNewItem] = useState("");
-//   const [items, setItems] = useState([]);
-
-//   // Helper functions
-//   function addItem() {
-//     if (!newItem) {
-//       alert("Enter an item");
-//       return;
-//     }
-//     const item = {
-//       id: Math.floor(Math.random() * 1000),
-//       value: newItem
-//     };
-
-//     setItems(oldList => [...oldList, item]);
-//     setNewItem("");
-//   }
-
-//   function deleteItem(id) {
-//     // console.log(id);
-//     const newArray = items.filter(item => item.id !== id);
-//     setItems(newArray);
-//   }
-
-//   return (
-//     <div className="App">
-//       {/* 1. Header */}
-//       <h1>Todo List App</h1>
-
-//       {/* 2. Input */}
-//       <input
-//         type="text"
-//         placeholder="Add an item" 
-//         value={newItem}
-//         onChange={e => setNewItem(e.target.value)}
-//       />
-
-//       <button onClick={() => addItem()}>Add</button>
-
-//       {/* 3. List of items  */}
-//       <ul>
-//         {items.map(item => {
-//           return(
-//             <li key={item.id}>{item.value} <button onClick={() => deleteItem(item.id)} >X</button></li> 
-//           )
-//         })}
-//       </ul>
-
-//     </div>
-//   );
-// }
-
-// export default App;
 import React, { useState } from 'react';
 import './App.css'; // Import Tailwind CSS
 
@@ -95,7 +36,7 @@ function App() {
         className="p-2 border border-gray-300 rounded mr-2"
       />
 
-      <div class="p-4">
+      <div className="p-4">
         <button onClick={() => addItem()} className="p-2 bg-blue-500 text-white rounded">Add</button>
       </div>
 
